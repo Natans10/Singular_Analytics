0bd253c9d298ad6c866f4042d0269e96a5a1e626
Natan Silva <natansilva1041@gmail.com>
2024-07-03 18:23:36 -0300

    03/07

---
+++ relatorios/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def teste(request):
+    return HttpResponse(request, "Templates/teste.html")
